fa1c128a85cf3066b7459a2c0d7c1cd8da9e71c1
lebedrvd <lebedkadima@yandex.ru>
2022-11-22 07:45:57 +0300

    changed

--- index.js
+++ index.js
@@ -1,121 +1,120 @@
 // 1st exercise
-const greetings = function sayHello(name) {
-  const firstLetter = name.charAt(0).toUpperCase();
-  const otherLettersLow = name.slice(1).toLowerCase();
-  if (typeof name === "string") {
-    if (`${firstLetter}${otherLettersLow}` == "Mark") {
-      return `Hi, Mark!`;
-    }
-    return `Hello, ${firstLetter}${otherLettersLow}!`;
+const sayHello = function (name) {
+  if (typeof name !== "string") {
+    return "Error";
   }
-  return "Wrong type of name";
+  const firstLetterBig = name[0].toUpperCase();
+  const otherLettersLow = name.slice(1).toLowerCase();
+  const nameChangedRegister = `${firstLetterBig}${otherLettersLow}`;
+  const userMark = `Mark`;
+  const isMark = userMark.toLowerCase() === name.toLowerCase();
+  const greeting = isMark ? "Hi" : "Hello";
+  return `${greeting}, ${nameChangedRegister}`;
 };
 
-console.log(greetings("MARK"));
+console.log(sayHello("maRK"));
 
 // 2nd exercise
-const hypotenuse = function calculations(n, m) {
-  if (typeof n === "number" && typeof m === "number" && n > 0 && m > 0) {
-    return Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
+const hypotenuse = function (n, m) {
+  if (typeof n !== "number" || typeof m !== "number" || n <= 0 || m <= 0) {
+    return `Wrong type of number`;
   }
-  return `Wrong type of number`;
+  return Math.sqrt(Math.pow(n, 2) + Math.pow(m, 2));
 };
 
-console.log(hypotenuse(3, 12));
+console.log(hypotenuse(3, 4));
 
 // 3rd exercise
-const minimum = function min(a, b) {
-  if (typeof a === "number" && typeof b === "number") {
-    if (a == b) {
-      return `They are equal. Min = ${a}`;
-    }
-    return Math.min(a, b);
+const minimum = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return `Wrong type of number`;
   }
-  return `Wrong type of number`;
+
+  if (a === b) {
+    return `They are equal. Min = ${a}`;
+  }
+  return Math.min(a, b);
 };
 console.log(minimum(-1, 5));
 
 // 4th exercise
 
-const check = function isEven(incomingNumber) {
+const isEven = function (incomingNumber) {
   if (typeof incomingNumber !== "number") {
     return "Wrong type of number";
   }
-  if (incomingNumber % 2 === 0) {
-    return "The number is even";
-  }
-  return "The number is odd";
+  return incomingNumber % 2 === 0;
 };
 
-console.log(check(14));
+console.log(isEven(12));
 
 // 5th exercise
 
-const newString = function deleteChars(incomingString) {
+const deleteChars = function (incomingString) {
   if (typeof incomingString !== "string") {
     return "Wrong type of variable";
   }
   return incomingString.slice(1, -1);
 };
 
-console.log(newString(" this is a string "));
+console.log(deleteChars(" this is a string "));
 
 // 6th exercise
-const result = function changedCases(word) {
+const changedCases = function (word) {
   if (typeof word !== "string" || word.length === 0) {
     return "Wrong type of variable or variable is empty";
   }
+  const firstLetterBig = word[0].toUpperCase();
+  const lastLetterBig = word.slice(-1).toUpperCase();
+  const restLettersWithOutLastLow = word.slice(1, -1).toLowerCase();
+  const restLettersLow = word.slice(1).toLowerCase();
   if (word.length % 2 === 0) {
-    return `${word[0].toUpperCase()}${word.slice(1, -1).toLowerCase()}${word
-      .slice(-1)
-      .toUpperCase()}`;
+    return `${firstLetterBig}${restLettersWithOutLastLow}${lastLetterBig}`;
   }
-
-  return `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`;
+  return `${firstLetterBig}${restLettersLow}`;
 };
 
-console.log(result("sKInda"));
+console.log(changedCases("sKInDa"));
 
 // 7th exercise
 
-const checkLetterCase = function compareLetterToLowerCase(letter) {
+const compareLetterToLowerCase = function (letter) {
   if (typeof letter !== "string" || letter.length !== 1) {
     return "Wrong type of variable or variable is incorrect";
   }
-  if (letter !== letter.toLowerCase()) {
-    return "Ты сам придумал фразу для большой буквы? Боже мой";
-  }
-  return "Нет уж, маленькие буквы - скучно";
+  const compareCases = letter === letter.toLowerCase();
+  const isBig = "Большая буква";
+  const isLow = "Маленькая буква";
+  const result = compareCases ? isLow : isBig;
+  return `${result}`;
 };
 
-console.log(checkLetterCase("F"));
+console.log(compareLetterToLowerCase("F"));
 
 // 8th exercise
 
-const twoStrings = function concatenate(firstString, secondString) {
+const concatenate = function (firstString, secondString) {
   if (typeof firstString !== "string" || typeof secondString !== "string") {
     return "Wrong type of variable";
   }
   return `${firstString} ${secondString}`;
 };
 
-console.log(twoStrings("Hello", "there"));
+console.log(concatenate("Hello", "there"));
 
 // 9th exercise
 
-const checkStringLength = function stringAndNumber(
-  incomingString,
-  incomingNumber
-) {
+const checkStringLength = function (incomingString, incomingNumber) {
   if (
     typeof incomingString !== "string" ||
     typeof incomingNumber !== "number"
   ) {
     return "Check the type of your variables";
   }
-  if (incomingString.length != incomingNumber) {
-    return "The length of your string is't equal to your number";
-  }
-  return `${incomingString}`;
+  const comparison = incomingString.length === incomingNumber;
+  const isEqual = `${incomingString}`;
+  const isNotEqual = "String is too long or too short";
+  const result = comparison ? isEqual : isNotEqual;
+  return `${result}`;
 };
 console.log(checkStringLength("look", 4));
